1a161b883159e5e146c2b8ba9b2864a3e9d99e9c
João Salvador <82125523+Ssalvador221@users.noreply.github.com>
2023-11-14 21:34:56 -0300

    Add files via upload

---
+++ BdScript/scriptBancoDeDados.sql
@@ -0,0 +1,14 @@
+create table estudante(
+	id int PRIMARY KEY auto_increment,
+    nome_completo varchar(80) not null,
+    idade int not null,
+    email varchar(80) not null,
+    endereco varchar(100) not null,
+    cep varchar(9),
+    telefone varchar(11),
+    usuario varchar(80) not null,
+    senha varchar(100) not null,
+	curso varchar(50) not null,
+    observacoes varchar(255),
+    ativo bool not null
+);
