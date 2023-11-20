675bf7a9db4e1c7dfcfec8bb28507ebccb417fd7
JOÃO SALVADOR RIZZO <joao_rizzo@estudante.sesisenai.org.br>
2023-11-20 19:35:29 -0300

    Feat: update project

---
+++ Crud/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.SwingUtilities;
+
+import Frames.AppFrame;
+
+public class Main{
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                exibirTela();
+            }
+        });
+    }
+
+    public static void exibirTela(){
+        AppFrame appFrame = new AppFrame();
+        appFrame.showScreen();
+    }
+
+}
---
+++ Frames/AppFrame.java
@@ -0,0 +1,13 @@
+package Frames;
+
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame {
+    public static final String TITULO = "CRUD Matrículas";
+
+
+    public void showScreen(){
+        pack();
+        setVisible(true);
+    }
+}
